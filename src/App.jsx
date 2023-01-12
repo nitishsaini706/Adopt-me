7b01a2acc6e2b0291add3990410b6ae49ee9951e
nitishsaini706 <nitishsaini706@gmail.com>
2023-01-12 16:40:07 +0530

    converting to JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Pet from './Pet';
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h1", {}, props.animal),
-    React.createElement("h1", {}, props.breed),
-  ]);
-};
+// const Pet = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, props.name),
+//     React.createElement("h1", {}, props.animal),
+//     React.createElement("h1", {}, props.breed),
+//   ]);
+// };
 
 // data flow in one side direction form parent to child that is App to pet
 // this one side data flow helps in debugging as we know pet cannot pass data
 // data came from app so we can debug our
 
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt-Me"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "dog",
+//       breed: "dashund",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Lucy",
+//       animal: "cat",
+//       breed: "stray",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Rajath",
+//       animal: "human",
+//       breed: "human",
+//     }),
+//   ]);
+// };
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt-Me"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "dog",
-      breed: "dashund",
-    }),
-    React.createElement(Pet, {
-      name: "Lucy",
-      animal: "cat",
-      breed: "stray",
-    }),
-    React.createElement(Pet, {
-      name: "Rajath",
-      animal: "human",
-      breed: "human",
-    }),
-  ]);
+
+  return (
+    <div>
+      <h1>Adopt-me</h1>
+      <Pet name ="Luna" animal="Dog" breed="Dashund"/>
+      <Pet name ="max" animal="Bird" breed="Beagle"/>
+      <Pet name ="Kasa" animal="Rajath" breed="Human"/>
+      
+    </div>
+  )
+  
 };
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+// root.render(React.createElement(App));
+root.render(<App/>)
